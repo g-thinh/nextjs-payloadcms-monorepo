91f0b5cfee9a2e0ee63df6848215cb7d77cb2a2b
Gia Thinh Nguyen <thinh-nguyen@hotmail.com>
2023-03-04 22:04:54 -0500

    add cors and csrf whitelist

--- cms/src/payload.config.ts
+++ cms/src/payload.config.ts
@@ -7,12 +7,15 @@ import Users from './collections/Users';
 import Media from './collections/Media';
 
 const serverURL = process.env.PAYLOAD_SERVER_URL;
+const clientURL = process.env.NEXT_CLIENT_URL;
 
 export default buildConfig({
   serverURL,
   admin: {
     user: Users.slug,
   },
+  cors: [serverURL, clientURL].filter(Boolean),
+  csrf: [serverURL, clientURL],
   collections: [Categories, Posts, Tags, Users, Media],
   typescript: {
     outputFile: path.resolve(__dirname, 'payload-types.ts'),
