d90b07d96066bc1fe4d4395422af413cc38833fb
Gia Thinh Nguyen <thinh-nguyen@hotmail.com>
2023-03-04 22:12:53 -0500

    create protected profile page

---
+++ web/src/pages/profile.tsx
@@ -0,0 +1,47 @@
+import { Article, Main, Section } from '@/components/Layout';
+import { User } from 'cms/src/payload-types';
+import { getCookie } from 'cookies-next';
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const { req, res } = context;
+  const payloadToken = getCookie('payload-token', { req, res });
+  const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/users/me`, {
+    credentials: 'include',
+    headers: {
+      Authorization: `JWT ${payloadToken}`,
+    },
+  });
+
+  const { user }: { user: User | null } = await response.json();
+
+  if (!user) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: 'false',
+      },
+    };
+  }
+
+  if (response.ok) {
+    return {
+      props: {
+        user,
+      },
+    };
+  }
+}
+
+export default function ProfilePage(props: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  return (
+    <Main>
+      <Article>
+        <Section>
+          <h2>My Profile</h2>
+          <pre>{JSON.stringify(props.user, null, 2)}</pre>
+        </Section>
+      </Article>
+    </Main>
+  );
+}
